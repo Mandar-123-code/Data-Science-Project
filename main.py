5fd7ef9a09dc8febece775a03edf683639523702
Mandar-123-code <kulkarnimandar702@gmail.com>
2025-02-09 12:01:50 +0530

    Project  ready

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
 from flask import Flask,render_template,request
+import pickle
 
 app = Flask(__name__)
 data = pd.read_csv('Cleaned_data.csv')
+pipe = pickle.load(open('RidgeModel.pkl','rb'))
 
 @app.route('/')
 def index():
@@ -11,5 +14,20 @@ def index():
 
     locations = sorted(data['location'].unique())
     return render_template('index.html',locations = locations)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    location =request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    sqft = request.form.get('total_sqft')
+
+    print(location,bhk,bath,sqft)
+    input = pd.DataFrame([[location,bhk,bath,sqft]],columns=['location','bhk','bath','total_sqft'])
+    prediction = pipe.predict(input)[0] * 100000 # To convert Lakhs into rupees
+
+
+    return str(np.round(prediction,2))
+
 if __name__ == "__main__":
     app.run(debug = True, port = 5001)
